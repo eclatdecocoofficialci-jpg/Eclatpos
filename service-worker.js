08b3f1b50dfda9c1a479536a97c8bca2537960e0
eclatdecocoofficialci-jpg <eclatdecocoofficialci@gmail.com>
2025-10-13 05:16:02 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,14 @@
-const CACHE_NAME = "eclat-pos-cache-v1";
+const CACHE_NAME = 'eclat-de-coco-v1';
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./manifest.json",
-  "./Icon-512.png"
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
